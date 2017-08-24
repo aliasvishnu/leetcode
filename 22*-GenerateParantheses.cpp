56cf3d66c794a7a44dd920f4bbba9717ff170642
aliasvishnu <aliasvishnu@gmail.com>
2017-08-23 18:58:43 -0700

    August23

---
+++ 22*-GenerateParantheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> answer;
+
+    void backtrack(int n, int open, int close, string s){
+        if(s.size() == 2*n) {
+            answer.push_back(s);
+            return;
+        }
+
+        if(open < n){
+            backtrack(n, open+1, close, s+"(");
+        }
+        if(close < open){
+            backtrack(n, open, close+1, s+")");
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        backtrack(n, 0, 0, s);
+        return answer;
+    }
+};
