067945b3d7a3a19348e7056dd1858e73422196ac
aliasvishnu <aliasvishnu@gmail.com>
2017-08-28 18:26:39 -0700

    Added some explanation

--- 75-SortColors.cpp
+++ 75-SortColors.cpp
@@ -4,6 +4,7 @@ public:
         int len = nums.size();
         int j = 0, k = len-1;
 
+        // i is where 0 should go, and k is where 2 should go, but as long as 2 in not there in k, i = k could be filled by 0 as well.
         for(int i = 0; i <= k; i++){
             if(nums[i] == 0){
                 int temp = nums[i];
