a395d794db59155b421a1d137a3b0b1c9f85b63b
aliasvishnu <aliasvishnu@gmail.com>
2017-11-01 19:52:17 -0700

    November1

--- 3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -6,6 +6,25 @@ public:
         int len = s.size();
         if(len == 0) return 0;
 
+        unordered_map<char, int> prev;
+        int start = 0, answer = -1;
+
+        for(int i = 0; i < len; i++){
+            if(prev.find(s[i]) == prev.end() || prev[s[i]] < start){
+                if(answer < i-start+1) answer = i-start+1;
+            }else{
+                start = prev[s[i]]+1;
+            }
+
+            prev[s[i]] = i;
+        }
+        return answer;
+    }
+
+    int lengthOfLongestSubstring(string s) {
+        int len = s.size();
+        if(len == 0) return 0;
+
         int start = 0, maxCount = -1000;
         for(int i = 0; i < len; i++){
             if(prev.find(s[i]) == prev.end()){
