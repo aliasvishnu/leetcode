f62cae736329520e7226e8afd827b246282d97b3
aliasvishnu <aliasvishnu@gmail.com>
2017-12-28 12:25:12 -0800

    Added hints Dec 28

--- Solutions/198-HouseRobber.cpp
+++ Solutions/198-HouseRobber.cpp
@@ -11,20 +11,15 @@ public:
         if(len == 0) return 0;
         if(len == 1) return nums[0];
         if(len == 2) return max(nums[1], nums[0]);
-        if(len == 3) return max(nums[1], nums[2]+nums[0]);
 
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-        dp[2] = max(nums[1], nums[2]+nums[0]);
 
-        for(int i = 3; i < len; i++){
-            dp[i] = max(dp[i-3], dp[i-2]) + nums[i];
-        }
-        int maxval = 0;
-        for(int i = 0; i < len; i++){
-            if(maxval < dp[i]) maxval = dp[i];
+        for(int i = 2; i < len; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
         }
 
-        return maxval;
+
+        return dp[len-1];
     }
 };
